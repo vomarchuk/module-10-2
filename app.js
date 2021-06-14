480c694f2c5042bd4106955a310ba877e52e463c
Volodymyr Marchuk <vomarchuk@Volodymyrs-MacBook-Air.local>
2021-06-14 21:03:14 +0200

    add markup manicure,pedicure, depilacja pactą cukrową

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 4444;
 
+const manicureData = require('./json/_manicure.json');
+const pedicureData = require('./json/_pedicure.json');
+const depilacjaPCData = require('./json/_depilacja-pasta-cukrowa.json');
+console.log(depilacjaPCData);
 // const products = require('./products.json'); подключяем файл JSON с данными
 
 const app = express();
@@ -20,15 +24,31 @@ app.get('/', (req, res) => {
   res.render('home', { cssFileName: 'home', title: 'Home' });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { cssFileName: 'about', title: 'About' });
+app.get('/manicure', (req, res) => {
+  res.render('manicure', {
+    manicureData,
+    cssFileName: 'style',
+    title: 'Manicure',
+  });
 });
 
-app.get('/products', (req, res) => {
-  //   res.render('products');
-  //   res.render('products', { products, .....cssFileName: 'products' });<------- одключяем файл JSON с данными
+app.get('/pedicure', (req, res) => {
+  res.render('pedicure', {
+    pedicureData,
+    cssFileName: 'style',
+    title: 'Pedicure',
+  });
+});
 
-  res.render('products', { cssFileName: 'products', title: 'Products' });
+app.get('/depilacjaPC', (req, res) => {
+  //   res.render('products');
+  //   res.render('products', { products, .....cssFileName: 'products' });<------- подключяем файл JSON с данными
+
+  res.render('depilacjaPC', {
+    depilacjaPCData,
+    cssFileName: 'style',
+    title: 'depilacja pastą-cukrową',
+  });
 });
 
 app.listen(PORT, () => {
